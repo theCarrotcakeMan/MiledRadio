eeb8fe9a04b98978a438ab2d40f7fb0aa02baec1
John Falcón <johngrowscode@gmail.com>
2020-06-11 20:46:07 -0500

    change(footer struct): Move player footer inside main container

--- www/src/components/mediaPlayer.vue
+++ www/src/components/mediaPlayer.vue
@@ -18,30 +18,30 @@
 		<article id="fullPlayerScreen" class="absolute w-full h-full p-6">
 			<img :src="coverUrl" :alt="stationLabel" class="block w-4/5 m-auto mt-16 mb-4">
 			<h3 class="inline-block mt-3 leading-loose text-lg text-center font-medium text-white">{{stationLabel}} <span class="stationTag_inline font-medium">{{stationAddr}}</span> </h3>
-		</article>
-	    <footer class="shadow-md fixed left-0 bottom-0 w-full h-auto p-6">
-			<nav id="stationLinks" class="block flex justify-center p-3">
-				<a class="inline-block align-middle mr-3 mb-3 text-grayoxford-light" target="_blank" onclick="window.open('https://www.facebook.com/MiledMexico', '_blank', 'location=yes')" title="Síguenos en Facebook"><span class="social facebook"></span></a>
-				<a class="inline-block align-middle mr-3 mb-3 text-grayoxford-light" target="_blank" onclick="window.open('https://www.twitter.com/noticiasmiled', '_blank', 'location=yes')" title="Síguenos en Twitter"><span class="social twitter"></span></a>
-				<a class="inline-block align-middle mr-3 mb-3 text-grayoxford-light" target="_blank" onclick="window.open('https://miled.com', '_blank', 'location=yes')"><span class="social webpage"></span></a>
-				<a class="inline-block align-middle mb-3 text-grayoxford-light" href="tel:+52 722 280 09 43"><span class="social phone"></span></a>
-			</nav>
+			<footer class="shadow-md fixed left-0 bottom-0 w-full h-auto p-6">
+			   <nav id="stationLinks" class="block flex justify-center p-3">
+				   <a class="inline-block align-middle mr-3 mb-3 text-grayoxford-light" target="_blank" onclick="window.open('https://www.facebook.com/MiledMexico', '_blank', 'location=yes')" title="Síguenos en Facebook"><span class="social facebook"></span></a>
+				   <a class="inline-block align-middle mr-3 mb-3 text-grayoxford-light" target="_blank" onclick="window.open('https://www.twitter.com/noticiasmiled', '_blank', 'location=yes')" title="Síguenos en Twitter"><span class="social twitter"></span></a>
+				   <a class="inline-block align-middle mr-3 mb-3 text-grayoxford-light" target="_blank" onclick="window.open('https://miled.com', '_blank', 'location=yes')"><span class="social webpage"></span></a>
+				   <a class="inline-block align-middle mb-3 text-grayoxford-light" href="tel:+52 722 280 09 43"><span class="social phone"></span></a>
+			   </nav>
 
-	        <nav class="flex justify-center">
-	            <a @click="resumeStream" v-show="paused" class="play text-grayoxford-light"><i class="material-icons text-4xl">play_arrow</i></a>
-	            <a @click="pauseStream" v-show="!paused" class="pause text-grayoxford-light"><i class="material-icons text-4xl">pause</i></a>
-	        </nav>
-			<div class="clearfix"></div>
-			<div v-if="!loading" class="block w-full p-1">
-				<vue-slider
-					v-model="volume"
-					tooltip="none"
-					:min="0"
-					:max="10"
-					>
-				</vue-slider>
-			</div>
-	    </footer>
+			   <nav class="flex justify-center">
+				   <a @click="resumeStream" v-show="paused" class="play text-grayoxford-light"><i class="material-icons text-4xl">play_arrow</i></a>
+				   <a @click="pauseStream" v-show="!paused" class="pause text-grayoxford-light"><i class="material-icons text-4xl">pause</i></a>
+			   </nav>
+			   <div class="clearfix"></div>
+			   <div v-if="!loading" class="block w-full p-1">
+				   <vue-slider
+					   v-model="volume"
+					   tooltip="none"
+					   :min="0"
+					   :max="10"
+					   >
+				   </vue-slider>
+			   </div>
+		   </footer>
+		</article>
 
 	</div>
 	</transition>
